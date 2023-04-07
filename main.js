e9e6df53db1191586de9244446c5e4579a7b7388
Ansh varma <122360981+anshvrma@users.noreply.github.com>
2023-04-07 14:49:08 +0530

    parallax

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 let text = document.getElementById('text');
 let pic1 = document.getElementById('pic1');
 let pic2 = document.getElementById('pic2');
+let pic3 = document.getElementById('pic3');
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
     text.style.marginTop = value * 2.5 + 'px';
-    pic2.style.top = value * -0.5 + 'px';
+    pic2.style.top = value * -0.1 + 'px';
+    pic3.style.right = value * -2 + 'px';
 
 });
